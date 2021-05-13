a556d5666d5facd79ebb35fc1cb9dfd1f2222570
Your Name <ilkinnazarli@gmail.com>
2021-05-13 20:37:38 +0400

    add post endpoint

--- npm-packages/index.js
+++ npm-packages/index.js
@@ -2,19 +2,26 @@
 'use strict';
 const express = require("express");
 const app = express();
+app.use(express.json());
 
 const courses = [
     { id: 1, name: "courses1" },
     { id: 2, name: "courses2" },
     { id: 3, name: "courses3" },
-]
-
-    ;
+];
 app.get('/', (req, res) => {
-    res.send('<H1>Ilkin Fidani sevirr !!!</H1>')
+    res.send('<H1>Hello World!!!</H1>')
 });
 app.get('/api/courses', (req, res) => {
     res.send(courses)
+});
+app.post('/api/courses', (req, res) => {
+    const course ={
+        id: courses.length +1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course)
 });
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id));
