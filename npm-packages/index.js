f140444e432275bfff2993b0f7ebb32b639d9560
Your Name <ilkinnazarli@gmail.com>
2021-05-13 22:46:24 +0400

    add validation vith joi modul

--- npm-packages/index.js
+++ npm-packages/index.js
@@ -1,9 +1,9 @@
 
 'use strict';
-const express = require("express");
+const Joi = require('joi')
+const express = require('express');
 const app = express();
 app.use(express.json());
-
 const courses = [
     { id: 1, name: "courses1" },
     { id: 2, name: "courses2" },
@@ -16,8 +16,16 @@ app.get('/api/courses', (req, res) => {
     res.send(courses)
 });
 app.post('/api/courses', (req, res) => {
-    const course ={
-        id: courses.length +1,
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    const result = Joi.validate(req.body,schema);
+    if (result.error) {
+        res.status(400).send(result.error.details[0].message);
+        return;
+    };
+    const course = {
+        id: courses.length + 1,
         name: req.body.name
     };
     courses.push(course);
