ed3d2957c9f46894c2c29a4c96f1cd1729408c9d
Your Name <ilkinnazarli@gmail.com>
2021-05-13 20:02:23 +0400

    add get  by id end point

--- npm-packages/index.js
+++ npm-packages/index.js
@@ -2,14 +2,24 @@
 'use strict';
 const express = require("express");
 const app = express();
+
+const courses = [
+    { id: 1, name: "courses1" },
+    { id: 2, name: "courses2" },
+    { id: 3, name: "courses3" },
+]
+
+    ;
 app.get('/', (req, res) => {
     res.send('<H1>Ilkin Fidani sevirr !!!</H1>')
 });
 app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3])
+    res.send(courses)
 });
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id)
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send(`The course with the given ID was not found.`);
+    res.send(course);
 });
-const port=process.env.PORT ||3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`listening on port ${port}.....`))
