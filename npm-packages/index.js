bec0b531661ab9c9d4dfa94fc09fbad779b05d1a
Your Name <ilkinnazarli@gmail.com>
2021-05-13 19:40:20 +0400

    get single item with api

--- npm-packages/index.js
+++ npm-packages/index.js
@@ -8,5 +8,8 @@ app.get('/', (req, res) => {
 app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3])
 });
+app.get('/api/courses/:id', (req, res) => {
+    res.send(req.params.id)
+});
 const port=process.env.PORT ||3000;
 app.listen(port, () => console.log(`listening on port ${port}.....`))
